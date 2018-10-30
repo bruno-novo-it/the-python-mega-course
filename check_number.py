d418283e4dd44325a8b80049a0765a569abd1497
Bruno Novo <brunonovo18@gmail.com>
2018-10-30 18:55:13 -0300

    Adding the first files

---
+++ check_number.py
@@ -0,0 +1,8 @@
+user_input = float(input("Enter a number(Ex. 100): "))
+
+if user_input > 100:
+    print("The number is Greater then 100")
+elif user_input == 100:
+    print("The number is equal to 100")
+else:
+    print("The number is Smaller the 100")
