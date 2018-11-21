b28f5513897f0520e2442a5d14b6072af27afd60
Bruno Novo <brunonovo18@gmail.com>
2018-11-21 08:33:20 -0200

    Adding erros.py file

---
+++ erros.py
@@ -0,0 +1,27 @@
+a = 1
+b = "2"
+
+print(int(2.5)
+print(a + b)
+
+print a
+
+print(c)
+
+print(c/0)
+
+
+# THIS WILL GENERATE A SYNTAX ERROR, BECAUSE DICTIONARIES ARE
+# MADE WITH (), NOT []--> LIST
+
+mydict = ["name":"John", "surname":"Smith"] 
+
+print(mydict) 
+
+
+# THIS WILL GENERATE A NAME ERROR, BECAUSE THE ELEMENTS INSIDE
+# THE LIST WERE NOT DEFINED, AND STRING ARE WITH QUOTES("")
+
+mylist = [John, Jack, Jim] 
+
+print(mylist)
