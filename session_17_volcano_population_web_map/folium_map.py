e7a39e3504d730cc307a6eadc5171ce226629824
Bruno <brunonovo18@gmail.com>
2020-12-02 20:21:04 -0300

    Finishing Session 17 from the course

--- session_17_volcano_population_web_map/folium_map.py
+++ session_17_volcano_population_web_map/folium_map.py
@@ -1,6 +1,27 @@
 import folium
 import subprocess
 import requests
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
+
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
 
 # Open a page with Chrome through Python
 #https://pythonprogramming.altervista.org/how-to-open-a-web-page-in-the-browser-using-chrome-with-python-and-webbrowser/
@@ -31,12 +52,30 @@ current_location = get_current_location()
 
 map = folium.Map(location=[current_location[2], current_location[3]], zoom_start=15, tiles = "Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
-fg.add_child(folium.Marker(location=[-23.59832,-46.64158], popup="Hi, I'm Home", icon=folium.Icon(color='red')))
+# Marke Layer
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=folium.Popup(iframe),
+        fill_color = color_producer(el), color = "grey", fill_opacity=0.7))
 
-map.add_child(fg)
+fgp = folium.FeatureGroup(name="Population")
 
-map.save("Map1.html")
+# Json Layer
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+    style_function=lambda x: {'fillColor':'yellow' if x['properties']['POP2005'] < 10000000
+    else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-openweb("chrome", ["Map1.html"])
+
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
+
+
+html_file="Map_html_popup_advanced.html"
+
+map.save(html_file)
+
+openweb("chrome", [html_file])
