8a56a213e22256728ba76241d3f5b760cf37b513
Bruno <brunonovo18@gmail.com>
2020-12-02 21:15:36 -0300

    Finishing Session 18 from the course

---
+++ session_18_fixing_programming_errors/errors.py
@@ -0,0 +1,14 @@
+# # Wrong
+# mydict = ["name":"John", "surname":"Smith"]
+
+# print(mydict)
+
+# # Correct(A Dictionary)
+# mydict = {"name":"John", "surname":"Smith"}
+
+# print(mydict)
+
+# # Wrong(Variables not Defined)
+# mylist = [John, Jack, Jim]
+
+# print(mylist)
