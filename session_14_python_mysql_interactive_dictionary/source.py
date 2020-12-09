78c924ddbe27b44459a87df61c9cbd6623acca10
Bruno <brunonovo18@gmail.com>
2020-12-09 19:10:46 -0300

    Finishing session 14 from the course

--- session_14_python_mysql_interactive_dictionary/source.py
+++ session_14_python_mysql_interactive_dictionary/source.py
@@ -19,3 +19,23 @@ if results:
         print(result[0])
 else:
     print("No word found!")
+
+
+
+### Get all rows where the value of the column Expression starts with r
+## "SELECT * FROM Dictionary WHERE Expression  LIKE 'r%'"
+
+### Get all rows where the value of the column Expression starts with rain
+## "SELECT * FROM Dictionary WHERE Expression  LIKE 'rain%'"
+
+### All rows where the length of the value of the column Expression is less than four characters
+## "SELECT * FROM Dictionary WHERE length(Expression) < 4"
+
+### All rows where the length of the value of the column Expression is four characters
+## "SELECT * FROM Dictionary WHERE length(Expression) = 4"
+
+### All rows where the length of the value of the column Expression is greater than 1 but less than 4 characters
+## "SELECT * FROM Dictionary WHERE length(Expression) > 1 AND length(Expression) < 4"
+
+### All rows of column Definition where the value of the column Expression starts with r
+## "SELECT Definition FROM Dictionary WHERE Expression  LIKE 'r%'"
